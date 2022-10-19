c522df492b3843c82dd0755067d67cb73afdc9b5
lenguyenkimngan <1851010077ngan@ou.edu.vn>
2022-10-19 17:31:20 +0700

    BTH2 cua Ngan

---
+++ SaleApp/SaleApp/index.py
@@ -0,0 +1,14 @@
+from flask import render_template
+
+from SaleApp import app, dao
+
+
+@app.route("/")
+def index():
+    categories = dao.load_categories()
+    products = dao.load_products()
+    return render_template('index.html', categories=categories, products=products)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
